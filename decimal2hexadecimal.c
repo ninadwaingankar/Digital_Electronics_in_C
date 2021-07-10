8eedc58c582a3c2c9a65c2b3cf56cfc9e67cf7e2
ninadwaingankar <ninadnw88@gmail.com>
2021-07-10 20:37:55 +0530

    Decimal to Hexadecimal conversion in C

---
+++ decimal2hexadecimal.c
@@ -0,0 +1,34 @@
+//decimal to hexadecimal program in C
+
+#include<stdio.h>
+#include<math.h>
+
+
+int main()
+{
+	int decimal;
+	printf("This program converts decimal number into binary & octal\n");
+	printf("Enter a decimal number\n");
+	scanf("%d",&decimal);
+	printf("Equivalent hexadecimal number of %d is %X\n",decimal,decimal);
+	return 0;
+}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
